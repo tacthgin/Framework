e4799ade16500718c628a27f89a9a4f24297a555
陈起慧 <821910196@qq.com>
2021-12-22 10:36:29 +0800

    命名修改

--- assets/GameFramework/Scripts/ToolLibary/Astar/AstarHelp/CrossAstarHelp.ts
+++ assets/GameFramework/Scripts/ToolLibary/Astar/AstarHelp/CrossAstarHelp.ts
@@ -5,14 +5,14 @@ import { IVec2 } from "../IVec2";
  * 走十字格子的辅助器
  */
 export class CrossAstarHelp implements IAstarHelp {
-    static s_crossAroundNodes: Array<IVec2> = new Array<IVec2>({ x: 0, y: -1 }, { x: 1, y: 0 }, { x: 0, y: 1 }, { x: -1, y: 0 });
+    static s_roundNodes: Array<IVec2> = new Array<IVec2>({ x: 0, y: -1 }, { x: 1, y: 0 }, { x: 0, y: 1 }, { x: -1, y: 0 });
 
     estimate(currentPosition: IVec2, endPosition: IVec2): number {
         return Math.abs(endPosition.y - currentPosition.y) + Math.abs(endPosition.x - currentPosition.x);
     }
 
     forEachAroundNodes(callbackfn: (position: IVec2) => void, thisArg?: any): void {
-        CrossAstarHelp.s_crossAroundNodes.forEach((position: IVec2) => {
+        CrossAstarHelp.s_roundNodes.forEach((position: IVec2) => {
             callbackfn.call(thisArg, position);
         });
     }
