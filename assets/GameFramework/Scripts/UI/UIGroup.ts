2a3a9e9426c2c9a080555978eb3a8a455ceb0a16
陈起慧 <821910196@qq.com>
2021-12-24 17:17:11 +0800

    界面组

--- assets/GameFramework/Scripts/UI/UIGroup.ts
+++ assets/GameFramework/Scripts/UI/UIGroup.ts
@@ -1,4 +1,6 @@
+import { GameFrameworkError } from "../Base/GameFrameworkError";
 import { GameFrameworkLinkedList } from "../Base/GameFrameworkLinkedList";
+import { ReferencePool } from "../Base/ReferencePool/ReferencePool";
 import { IUIForm } from "./IUIForm";
 import { IUIGroup } from "./IUIGroup";
 import { IUIGroupHelp } from "./IUIGroupHelp";
@@ -61,22 +63,112 @@ export class UIGroup implements IUIGroup {
     hasUIForm(serialIdOrUIFormAssetName: string | number): boolean {
         if (typeof serialIdOrUIFormAssetName === "number") {
             for (let uiFormInfo of this._uiFormInfos) {
-                
+                if (uiFormInfo.uiForm.serialId === serialIdOrUIFormAssetName) {
+                    return true;
+                }
+            }
+        } else {
+            if (!serialIdOrUIFormAssetName) {
+                throw new GameFrameworkError("ui form asset name is invalid");
+            }
+            for (let uiFormInfo of this._uiFormInfos) {
+                if (uiFormInfo.uiForm.uiFormAssetName === serialIdOrUIFormAssetName) {
+                    return true;
+                }
             }
         }
+
+        return false;
     }
 
     getUIForm(serialIdOrUIFormAssetName: string | number): IUIForm | null {
-        throw new Error("Method not implemented.");
+        if (typeof serialIdOrUIFormAssetName === "number") {
+            for (let uiFormInfo of this._uiFormInfos) {
+                if (uiFormInfo.uiForm.serialId === serialIdOrUIFormAssetName) {
+                    return uiFormInfo.uiForm;
+                }
+            }
+        } else {
+            if (!serialIdOrUIFormAssetName) {
+                throw new GameFrameworkError("ui form asset name is invalid");
+            }
+            for (let uiFormInfo of this._uiFormInfos) {
+                if (uiFormInfo.uiForm.uiFormAssetName === serialIdOrUIFormAssetName) {
+                    return uiFormInfo.uiForm;
+                }
+            }
+        }
+
+        return null;
     }
 
     getUIForms(uiFormAssetName: string): IUIForm[] {
-        throw new Error("Method not implemented.");
+        if (!uiFormAssetName) {
+            throw new GameFrameworkError("ui form asset name is invalid");
+        }
+        let uiForms: IUIForm[] = [];
+        for (let uiFormInfo of this._uiFormInfos) {
+            if (uiFormInfo.uiForm.uiFormAssetName === uiFormAssetName) {
+                uiForms.push(uiFormInfo.uiForm);
+            }
+        }
+        return uiForms;
     }
 
     getAllUIForms(): IUIForm[] {
-        throw new Error("Method not implemented.");
+        let uiForms: IUIForm[] = [];
+        for (let uiFormInfo of this._uiFormInfos) {
+            uiForms.push(uiFormInfo.uiForm);
+        }
+        return uiForms;
     }
 
-    private refresh() {}
+    addUIForm(uiForm: IUIForm): void {
+        this._uiFormInfos.addFirst(UIFormInfo.create(uiForm));
+    }
+
+    removeUIForm(uiForm: IUIForm): void {
+        let uiFormInfo: UIFormInfo | null = this.getUIFormInfo(uiForm);
+        if (!uiFormInfo) {
+            throw new GameFrameworkError(`can't not find ui form info serialId: ${uiForm.serialId} asset name: ${uiForm.uiFormAssetName}`);
+        }
+
+        if (!uiFormInfo.covered) {
+            uiFormInfo.covered = true;
+            uiForm.onCover();
+        }
+
+        if (!uiFormInfo.paused) {
+            uiFormInfo.paused = true;
+            uiForm.onPause();
+        }
+
+        if (!this._uiFormInfos.remove(uiFormInfo)) {
+            throw new GameFrameworkError(`UI group ${this._name} not exists specified UI form [${uiForm.serialId}]${uiForm.uiFormAssetName}`);
+        }
+
+        ReferencePool.release(uiFormInfo);
+    }
+
+    refocusUIForm(uiForm: IUIForm) {
+        let uiFormInfo: UIFormInfo | null = this.getUIFormInfo(uiForm);
+        if (!uiFormInfo) {
+            throw new GameFrameworkError(`can't not find ui form info serialId: ${uiForm.serialId} asset name: ${uiForm.uiFormAssetName}`);
+        }
+        this._uiFormInfos.remove(uiFormInfo);
+        this._uiFormInfos.addFirst(uiFormInfo);
+    }
+
+    private refresh(): void {
+        
+    }
+
+    private getUIFormInfo(uiForm: IUIForm): UIFormInfo | null {
+        for (let uiFormInfo of this._uiFormInfos) {
+            if (uiFormInfo.uiForm === uiForm) {
+                return uiFormInfo;
+            }
+        }
+        return null;
+    }
 }
